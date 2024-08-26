60fcc2e804925e9ae197a824159cb169b39dcdea
Kanet <zombiedelah@gmail.com>
2024-08-26 17:01:27 +0900

    re-export 'signature'

--- sequencer/src/lib.rs
+++ sequencer/src/lib.rs
@@ -1,17 +1,14 @@
 #[cfg(any(feature = "full", feature = "block-commitment"))]
 pub use block_commitment;
-
 #[cfg(any(feature = "full", feature = "context"))]
 pub use context;
-
 #[cfg(any(feature = "full", feature = "json-rpc"))]
 pub use json_rpc;
-
 #[cfg(any(feature = "full", feature = "kvstore"))]
 pub use kvstore;
-
 #[cfg(feature = "liveness-evm")]
 pub use liveness_evm as liveness;
-
+#[cfg(feature = "signature")]
+pub use signature;
 #[cfg(feature = "validation-eigenlayer")]
 pub use validation_eigenlayer as validation;
