f6853ab1ea74bc7a512b15443d4c210fc628485d
kanet <zombiedelah@gmail.com>
2024-09-01 14:56:13 +0900

    Resolve: https://github.com/radiusxyz/radius-sdk-rs/issues/33

--- sequencer/src/lib.rs
+++ sequencer/src/lib.rs
@@ -1,17 +1,12 @@
 #[cfg(any(feature = "full", feature = "block-commitment"))]
 pub use block_commitment;
-
 #[cfg(any(feature = "full", feature = "context"))]
 pub use context;
-
 #[cfg(any(feature = "full", feature = "json-rpc"))]
 pub use json_rpc;
-
 #[cfg(any(feature = "full", feature = "kvstore"))]
 pub use kvstore;
-
 #[cfg(feature = "liveness-evm")]
-pub use liveness_evm as liveness;
-
+pub use liveness_evm;
 #[cfg(feature = "validation-eigenlayer")]
-pub use validation_eigenlayer as validation;
+pub use validation_eigenlayer;
