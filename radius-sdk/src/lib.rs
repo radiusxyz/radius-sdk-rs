0c26b3a500ae068818033b01be0aa4664595aadd
kanet1105 <zombiedelah@gmail.com>
2025-01-09 22:03:18 +0900

    Export 'json-rpc', 'liveness', 'validation' as modules.

--- radius-sdk/src/lib.rs
+++ radius-sdk/src/lib.rs
@@ -1,16 +1,34 @@
-pub mod util;
-
 #[cfg(any(feature = "full", feature = "context"))]
 pub use context;
-#[cfg(any(feature = "full", feature = "json-rpc"))]
-pub use json_rpc;
+#[cfg(any(
+    feature = "full",
+    feature = "json-rpc-client",
+    feature = "json-rpc-server"
+))]
+pub mod json_rpc {
+    #[cfg(any(feature = "full", feature = "json-rpc-client"))]
+    pub use json_rpc_client as client;
+    #[cfg(any(feature = "full", feature = "json-rpc-server"))]
+    pub use json_rpc_server as server;
+}
 #[cfg(any(feature = "full", feature = "kvstore-bytes", feature = "kvstore-json"))]
 pub use kvstore;
 #[cfg(any(feature = "full", feature = "liveness-radius"))]
-pub use liveness_radius;
+pub mod liveness {
+    #[cfg(any(feature = "full", feature = "liveness-radius"))]
+    pub use liveness_radius as radius;
+}
 #[cfg(any(feature = "full", feature = "signature"))]
 pub use signature;
-#[cfg(any(feature = "full", feature = "validation-eigenlayer"))]
-pub use validation_eigenlayer;
-#[cfg(any(feature = "full", feature = "validation-symbiotic"))]
-pub use validation_symbiotic;
+pub mod util;
+#[cfg(any(
+    feature = "full",
+    feature = "validation-eigenlayer",
+    feature = "validation-symbiotic"
+))]
+pub mod validation {
+    #[cfg(any(feature = "full", feature = "validation-eigenlayer"))]
+    pub use validation_eigenlayer as eigenlayer;
+    #[cfg(any(feature = "full", feature = "validation-symbiotic"))]
+    pub use validation_symbiotic as symbiotic;
+}
